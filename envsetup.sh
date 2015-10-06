1abfd9db81a148e981fa9ba73c7d362fff0d9a8d
Chet Kener <Cl3Kener@gmail.com>
2015-10-06 08:22:36 -0400

    Remove Default Targets
    * I don't use these!
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -551,9 +551,6 @@ function add_lunch_combo()
     LUNCH_MENU_CHOICES=(${LUNCH_MENU_CHOICES[@]} $new_combo)
 }
 
-# add the default one here
-add_lunch_combo aosp_arm-eng
-
 function print_lunch_menu()
 {
     local uname=$(uname)
