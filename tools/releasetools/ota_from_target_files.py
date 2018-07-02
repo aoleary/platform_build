LineageOS/android_build

Add build.prop to incrementals too
Change-Id: I0ef89210ba7dd05a7cff5c6b56687bfc60add7cb

tools/releasetools/ota_from_target_files.py
@@ -933,6 +933,8 @@ endif;
   metadata["ota-required-cache"] = str(script.required_cache)
   WriteMetadata(metadata, output_zip)
 
+  common.ZipWriteStr(output_zip, "system/build.prop",
+                     ""+input_zip.read("SYSTEM/build.prop"))
 
 def WriteVerifyPackage(input_zip, output_zip):
   script = edify_generator.EdifyGenerator(3, OPTIONS.info_dict)

aoleary/platform_build

Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

tools/releasetools/ota_from_target_files.py
@@ -997,6 +997,8 @@ endif;
   metadata["ota-required-cache"] = str(script.required_cache)
   WriteMetadata(metadata, output_zip)
 
+  common.ZipWriteStr(output_zip, "system/build.prop",
+                     ""+target_zip.read("SYSTEM/build.prop"))
 
 def WriteVerifyPackage(input_zip, output_zip):
   script = edify_generator.EdifyGenerator(3, OPTIONS.info_dict)